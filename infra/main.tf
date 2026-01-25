b63a8daed5ee46969535ce9f6560455d7d4d01c2
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-25 18:48:07 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,56 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "app_key" {
+  key_name   = "fastapi-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "app_sg" {
+  name = "fastapi-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP/32"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_ec2" {
+  ami                    = var.ami_id
+  instance_type          = "t3a.micro"
+  key_name               = aws_key_pair.app_key.key_name
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "FastAPI-Docker-EC2"
+  }
+}
 
