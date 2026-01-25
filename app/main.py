bbf249058b46816bea0b0a5df8c7a19edfb16483
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-25 17:12:55 -0500

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from db import save_user
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/login", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html",
+        {"request": request, "message": ""})
+
+@app.post("/login", response_class=HTMLResponse)
+def login(request: Request,
+          username: str = Form(...),
+          password: str = Form(...)):
+    save_user(username, password)
+    return templates.TemplateResponse("login.html",
+        {"request": request, "message": "User saved successfully"})
