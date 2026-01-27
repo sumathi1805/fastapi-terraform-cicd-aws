3e902c4ab37eae9b5ee74d9ed352aaaf267c9703
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-27 18:35:41 -0500

    Update user_data.sh

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-yum update -y
-
-# Install Docker
-amazon-linux-extras install docker -y
+yum install -y docker
 systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
 
-# Install Docker Compose (v2)
-curl -L "https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64" \
-  -o /usr/local/bin/docker-compose
-
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+-o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+mkdir -p /opt/app
+cd /opt/app
+
+# get secrets
+aws ssm get-parameter --name /fastapi_app/nysqluser --with-decryption --query Parameter.Value --output text > .env
+aws ssm get-parameter --name /fastapi_app/mysqlpasswd --with-decryption --query Parameter.Value --output text >> .env
+aws ssm get-parameter --name /fastapi_app/mysqldb --with-decryption --query Parameter.Value --output text >> .env
+
+docker compose -f docker-compose.runtime.yml pull
+docker compose -f docker-compose.runtime.yml up -d
+
