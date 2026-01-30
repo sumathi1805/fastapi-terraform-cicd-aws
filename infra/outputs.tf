ec4cd065bb6610cc52f22a56799185db8306873c
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-30 14:09:53 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,14 @@
-output "ec2_public_ip" {
-  value = aws_instance.app_ec2.public_ip
+# EC2 Instance ID
+output "ec2_instance_id" {
+  value = length(aws_instance.app_ec2) > 0 ? aws_instance.app_ec2[0].id : data.aws_instances.existing_ec2.ids[0]
 }
+
+# EC2 Public IP
+output "ec2_public_ip" {
+  value = length(aws_instance.app_ec2) > 0 ? aws_instance.app_ec2[0].public_ip : data.aws_instance.existing_ec2_info[0].public_ip
+}
+
+# Security Group ID
 output "security_group_id" {
-  description = "ID of the security group"
-  value       = aws_security_group.app_sg.id
+  value = aws_security_group.app_sg.id
 }
