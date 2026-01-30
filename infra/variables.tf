92d47421c8db5e24751ac55494bf48bf79c2f2b6
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-30 13:09:36 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,8 @@ variable "ami_id" {
   type        = string
   default     = "ami-0532be01f26a3de55"  # replace with your AMI ID
 }
+
+variable "environment" {
+  description = "Environment name (dev/prod)"
+  default     = "dev"
+}
