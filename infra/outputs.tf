ef9ceed851ace840752f86a3d0df764762e3fff1
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-30 13:11:10 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,7 @@
 output "ec2_public_ip" {
   value = aws_instance.app_ec2.public_ip
 }
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.app_sg.id
+}
