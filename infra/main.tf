7f288c76d0e14ac96daf09c23e4acaac1dbfda50
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-01-31 15:06:53 -0500

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "terraform-state-bucket-us-east-1"       # replace with your S3 bucket
+    key            = "fastapi/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_security_group" "app_sg" {
+  name_prefix = "fastapi-sg-"          # avoids duplicate name error
+  description = "Security group for FastAPI app"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_iam_instance_profile" "existing_profile" {
+  name = "ec2roleforcodedeploy_cicd"  # usually same as role name
+}
+
+
+
+resource "aws_instance" "app_ec2" {
+  ami                    = var.ami_id
+  instance_type          = "t3a.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  iam_instance_profile = data.aws_iam_instance_profile.existing_profile.name
+
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "FastAPI-Docker-EC2"
+    Env  = var.environment
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [
+      user_data,
+      ami,
+      tags
+    ]
+  }
+
+}
