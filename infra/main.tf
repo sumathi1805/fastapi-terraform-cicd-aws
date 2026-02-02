6dc8a8dad1d6d5fda3abdc7300e9c61d44a573ad
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-02-01 19:44:12 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -39,7 +39,12 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 
   egress {
