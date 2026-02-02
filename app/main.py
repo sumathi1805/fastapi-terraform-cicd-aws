3ea55e21385f8fbef2fb7c015a694e8576d088fe
Sumathi Shanmugam <150107821+sumathi1805@users.noreply.github.com>
2026-02-01 20:51:54 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,4 +21,4 @@ def login(request: Request,
           password: str = Form(...)):
     save_user(username, password)
     return templates.TemplateResponse("login.html",
-        {"request": request, "message": "User details saved successfully.... Thanks for adding"})
+        {"request": request, "message": "User details saved successfully!! Thanks for adding"})
